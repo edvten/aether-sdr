c8dccb0fb2645aaf08987230dca3dfb5d244b185
Zelmyx <>
2025-12-13 16:18:45 +0100

    feat: add FM demodulation with boxcar decimation and IIR de-emphasis
    Output audio can be played with the following command:
    aplay -t raw -f S16_LE -c 1 -r 48000 radio_output.raw

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <algorithm>
+#include <cmath>
+#include <complex>
 #include <cstdint>
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <rtl-sdr.h>
 #include <stdexcept>
@@ -49,6 +53,7 @@ public:
       std::cerr << "Warning: Short read (" << bytes_read << "bytes)\n";
     }
   }
+
 public:
   // DEFAULT_BUF_LENGTH in rtl_sdr.c source code
   static constexpr int BUF_SIZE = 16 * 16384;
@@ -57,23 +62,131 @@ private:
   rtlsdr_dev_t *dev = nullptr;
 };
 
-int main() {
-  try {
-    SdrDevice sdr(0);
+class AudioProcessor {
+private:
+  // Previous IQ sample
+  std::complex<float> prev_sample;
+  // Previous De-emphasised sample
+  float previous_filtered_sample;
 
-    sdr.configure(2048000, 99300000, 35);
+  // Decimation moving average variables
+  int decimation_counter;
+  float decimation_sum;
+  // Based on sample rate 1.92 MHz and audio sample rate 48KHz
+  const int DECIMATION_RATE = 40;
 
-    std::vector<uint8_t> buffer(SdrDevice::BUF_SIZE);
+  // constant for de-emphasis in europe
+  const float alpha = 0.34;
 
-    sdr.read_sync(buffer);
+public:
+  AudioProcessor() {
+    prev_sample = std::complex<float>(1.0f, 0.0);
+    previous_filtered_sample = 0.0f;
 
-    for (const auto &byte : buffer) {
-      std::cout << static_cast<int>(byte) << "\n";
+    // Simple moving average (boxcar) filter initialisation
+    decimation_counter = 0;
+    decimation_sum = 0.0f;
+  }
+  std::vector<int16_t> process(const std::vector<uint8_t> &raw_iq) {
+    // IQ sampling gives us the factor 2.
+    // We accumulate DECIMATION_RATE samples and filter them to become 1
+    // Hence our output buffer is smaller than the input buffer by a factor of
+    // (2 * DECIMATION_RATE)
+    std::vector<int16_t> output_buffer;
+    output_buffer.reserve(raw_iq.size() / (2 * DECIMATION_RATE));
+
+    // Note i += 2 since we jump from I sample to I sample
+    for (size_t i = 0; i < raw_iq.size(); i += 2) {
+      // Convert uint8_t sample to float:
+      // https://k3xec.com/packrat-processing-iq/
+      float real = ((float)raw_iq[i] - 127.5f) / 127.5f;
+      float imag = ((float)raw_iq[i + 1] - 127.5f) / 127.5f;
+      std::complex<float> current_sample = std::complex<float>(real, imag);
+
+      // We only care about the change in phase from the previous sample.
+      // Hence, we can perform complex multiplication with the complex conjugate
+      // of the previous sample to create a new complex number who's phase is
+      // the difference between the current sample and the previous sample:
+      // r1 * e^(i*p1) * conj(r2 * e^(i*p2)) = r1 * e^(i*p1) * r2 * e^(-i*p2) =
+      // = r1 * r2 * e^(i(p1 - p2)). arctan is then used to extract this phase.
+      std::complex<float> delta_sample =
+          current_sample * std::conj(prev_sample);
+      float delta_phase = std::atan2(delta_sample.imag(), delta_sample.real());
+
+      // Remember the current sample
+      prev_sample = current_sample;
+
+      // Moving average filter update
+      decimation_sum += delta_phase;
+      decimation_counter++;
+
+      // If we have collected DECIMATION_RATE samples
+      if (decimation_counter == DECIMATION_RATE) {
+        // Calculate the average value
+        float audio_sample = decimation_sum / (float)DECIMATION_RATE;
+
+        // Reset decimation counters and sum
+        decimation_counter = 0;
+        decimation_sum = 0.0f;
+
+        // de-emphasis like in below:
+        // rtl_fm.c: void deemph_filter(struct demod_state *fm)
+        float filtered_sample = (alpha * audio_sample) +
+                                ((1.0f - alpha) * previous_filtered_sample);
+        previous_filtered_sample = filtered_sample;
+
+        // Amplify the filtered audio sample
+        float amplified_sample = filtered_sample * 16000.0f;
+        // Clamp values to prevent integer overflow when casting to int16
+        amplified_sample = std::clamp(amplified_sample, -32768.0f, 32767.0f);
+
+        // Append to our output buffer
+        output_buffer.push_back(static_cast<int16_t>(amplified_sample));
+      }
     }
-  } catch (const std::exception &e) {
-    std::cerr << "Fatal Error: " << e.what() << std::endl;
-    return 1;
+
+    return output_buffer;
+  }
+};
+
+int main() {
+  SdrDevice sdr(0);
+
+  int sample_rate = 1920000; // 1.92 MHz
+  int frequency = 98400000;  // 98.4 MHz
+  int gain_db = 35;          // 35 db
+
+  sdr.configure(sample_rate, frequency, gain_db);
+
+  float seconds_to_record = 10.0f;
+  // Each sample contains two bytes (IQ)
+  long long total_bytes_needed =
+      (long long)(sample_rate * 2 * seconds_to_record);
+
+  // This ensures that we allocate enough OR more space in our buffer
+  // Only doing total_bytes_needed / SdrDevice::BUF_SIZE could mean
+  // that we don't allocate enough space
+  int buffer_ratio =
+      (total_bytes_needed + SdrDevice::BUF_SIZE - 1) / SdrDevice::BUF_SIZE;
+
+  std::vector<uint8_t> intermediate_buffer(SdrDevice::BUF_SIZE);
+  std::vector<uint8_t> buffer;
+  buffer.reserve(SdrDevice::BUF_SIZE * buffer_ratio);
+
+  for (int i = 0; i < buffer_ratio; i++) {
+    sdr.read_sync(intermediate_buffer);
+    buffer.insert(buffer.end(), intermediate_buffer.begin(),
+                  intermediate_buffer.end());
   }
 
+  AudioProcessor AP;
+  std::vector<int16_t> audio = AP.process(buffer);
+
+  std::ofstream out_file;
+  out_file.open("radio_output.raw", std::ios::binary);
+
+  out_file.write(reinterpret_cast<const char *>(audio.data()),
+                 audio.size() * sizeof(int16_t));
+  out_file.close();
   return 0;
 }
